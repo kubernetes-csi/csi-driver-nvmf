847c06d6c6cc9fa4a066f270a482447da62f0c2e
zhouenhua <zhouenhua@bytedance.com>
2021-10-27 17:54:52 +0800

    Update boilerplate text and OWNER_ALIASES
    Signed-off-by: zhouenhua <zhouenhua@bytedance.com>

--- pkg/nvmf/nvmf_utils.go
+++ pkg/nvmf/nvmf_utils.go
@@ -1,17 +1,35 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package nvmf
 
 import (
-	"csi-driver-nvmf/pkg/utils"
 	"context"
 	"fmt"
-	"github.com/kubernetes-csi/csi-lib-utils/protosanitizer"
-	log "github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
-	"k8s.io/klog"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/kubernetes-csi/csi-lib-utils/protosanitizer"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"k8s.io/klog"
+
+	"csi-driver-nvmf/pkg/utils"
 )
 
 func waitForPathToExist(devicePath string, maxRetries, intervalSeconds int, deviceTransport string) (bool, error) {
